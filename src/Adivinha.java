1421e3fbebf36fb940133cb7878a64a7b67cdc2e
Natália Doreto <dorettto@icloud.com>
2025-04-02 16:51:04 -0300

    Initial commit

---
+++ src/Adivinha.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Adivinha {    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int Numero = (int)(Math.random() * 100 + 1); // Cria um número aleatório e o transforma em inteiro
+        // Página 53
+        entrada.close();
+    }
+}
